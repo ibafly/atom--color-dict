1ae20d8178ad1c89e16719ac0838783b56663473
iba <copppe@gmail.com>
2019-05-03 23:45:29 +0800

    fix: regex that matches all hex in a hexes-only line && pass a assertion

--- test/color-dict.test.js
+++ test/color-dict.test.js
@@ -1,13 +1,22 @@
 /* eslint-env mocha */
 
-import colorDict from '../lib/color-dict'
-import chai from 'chai'
+import utils from '../lib/utils'
+import { convertToVarsPairs,
+  retrieveInSelections,
+  retrieveOnCursors
+} from '../lib/differentConvert'
 
+import chai from 'chai'
 chai.should()
 // chai.use(require('chai-things'))
 
-describe('unit test', function() {
-  it('#HEX to color name', function() {
-    (colorDict.getOutput('#2299dd')).should.be.equal('Home World');
-  });
-});
+describe('unit test', function () {
+  it('#HEX to color name', function () {
+    convertToVarsPairs('#2299dd').should.eql('Home World')
+  })
+  it('match a #HEX array in a hexes-only line', function () {
+    const line = '   #2299dd  #2299dd#dd9922#29d '
+
+    utils.getHexesInAHexesOnlyLine(line).should.eql(['#2299dd', '#2299dd', '#dd9922', '#29d'])
+  })
+})
