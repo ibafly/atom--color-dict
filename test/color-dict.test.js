2ec1ff20ab14123eb9c0b7a6e6d829784de6bf96
iba <copppe@gmail.com>
2019-05-08 17:29:27 +0800

    test: fn to match all hexes in hexes only line

--- test/color-dict.test.js
+++ test/color-dict.test.js
@@ -11,12 +11,29 @@ chai.should()
 // chai.use(require('chai-things'))
 
 describe('unit test', function () {
-  it('#HEX to color name', function () {
-    convertToVarsPairs('#2299dd').should.eql('Home World')
-  })
+//   it('#HEX to color name', function () {
+//     const RangeObj1 = [{
+//       content:
+// `   #2299dd  #2299dd#dd9922  #29d
+//     #111 #222 here comes none-hex words after a hex
+//     now some before hexes #333      #444#555
+//     #666 #777in between#888     #999`,
+//       start: { row: 16, column: 4 },
+//       end: { row: 19, column: 36 }
+//     }]
+//
+//     convertToVarsPairs(RangeObj1, 'source.css.scss').should.equal('Home World')
+//   })
   it('match a #HEX array in a hexes-only line', function () {
-    const line = '   #2299dd  #2299dd#dd9922#29d '
+    const line1 = '  #2299dd  #2299dd#dd9922  #29d   '
+    const line2 = '#111 #222 here comes none-hex words after a hex'
+    const line3 = 'now some before hexes #333      #444#555  '
+    const line4 = '#666 #777in between#888     #999'
 
-    utils.getHexesInAHexesOnlyLine(line).should.eql(['#2299dd', '#2299dd', '#dd9922', '#29d'])
+    utils.getHexesInAHexesOnlyLine(line1).should.eql(['#2299dd', '#2299dd', '#dd9922', '#29d'])
+    utils.getHexesInAHexesOnlyLine(line2).should.equal(line2)
+    utils.getHexesInAHexesOnlyLine(line3).should.equal(line3)
+    utils.getHexesInAHexesOnlyLine(line4).should.equal(line4)
+
   })
 })
